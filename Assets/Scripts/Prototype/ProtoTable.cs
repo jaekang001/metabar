36efe75b049f00a2c5d9ed05e679b3c58e3be2ea
ChartaP <charta97@naver.com>
2023-03-30 20:02:39 +0900

    ProtoTable
    프로토타입의 테이블 씬을 작업하였습니다.
    필요한 스프라이트와 스크립트 배치

---
+++ Assets/Scripts/Prototype/ProtoTable.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoTable : MonoBehaviour
+{
+    [SerializeField]
+    private Transform toolPoint;
+    [SerializeField]
+    private Transform glassPoint;
+    [SerializeField]
+    private Transform drinkPoint;
+
+    [SerializeField]
+    private GameObject[] tools;
+    [SerializeField]
+    private GameObject[] glasses;
+    [SerializeField]
+    private GameObject[] drinks;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ToolSet(Boolean a,Boolean b, Boolean c)
+    {
+        
+    }
+
+    private void GlassSet(Boolean a, Boolean b)
+    {
+
+    }
+
+    private void DrinkSet(Boolean a, Boolean b) { 
+    
+    }
+    
+}
