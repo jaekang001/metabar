f15708d3a9bcf80de4e388013b750706234e4881
ChartaP <charta97@naver.com>
2023-04-12 23:28:02 +0900

    Add WaterShader
    액체의 높낮이와 수면 효과를 포함하는 액체 쉐이더 추가

--- Assets/Scripts/Prototype/ProtoTable.cs
+++ Assets/Scripts/Prototype/ProtoTable.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using static UnityEngine.GridBrushBase;
 
 public class ProtoTable : MonoBehaviour
 {
@@ -23,7 +25,9 @@ public class ProtoTable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ToolSet(new int[] { 0,1});
+        GlassSet(0);
+        DrinkSet(new int[] { 0, 1 ,2});
     }
 
     // Update is called once per frame
@@ -32,18 +36,31 @@ public class ProtoTable : MonoBehaviour
         
     }
 
-    private void ToolSet(Boolean a,Boolean b, Boolean c)
+    private void ToolSet(int[] toolIDs)
     {
-        
+        Vector3 pos = toolPoint.position;
+        foreach (int toolID in toolIDs) {
+
+            Instantiate(this.tools[toolID], pos, Quaternion.identity, toolPoint);
+            pos += new Vector3(0,-2,-1);
+
+        }
     }
 
-    private void GlassSet(Boolean a, Boolean b)
+    private void GlassSet(int glassID)
     {
-
+        Instantiate(this.glasses[glassID], glassPoint.position, Quaternion.identity, glassPoint);
     }
 
-    private void DrinkSet(Boolean a, Boolean b) { 
-    
+    private void DrinkSet(int[] drinkIDs) {
+        Vector3 pos = drinkPoint.position;
+        foreach (int drinkID in drinkIDs)
+        {
+
+            Instantiate(this.drinks[drinkID], pos, Quaternion.identity, drinkPoint);
+            pos += new Vector3(1, -1, -1);
+
+        }
     }
     
 }
